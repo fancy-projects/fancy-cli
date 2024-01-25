a7bce13f2eba112c7444585284d92f4c2915eac3
Zhisen <zhisen.urgolites@gmail.com>
2024-01-24 19:51:25 -0800

    bugfixes

--- src/installer.sh
+++ src/installer.sh
@@ -6,16 +6,15 @@ set -e
 
 cd /usr/local/share
 
-rm -rf fancy-cli
+rm -rf fancy-cli ; git clone https://github.com/pythonkid90/fancy-cli.git
 
-git clone https://github.com/pythonkid90/fancy-cli.git
 pip3 install ./fancy-cli
 
-ls ./fancy-cli
+
 cp ./fancy-cli/fancy-exe .
-ls
+
 rm -rf fancy-cli
 
-rm /usr/local/bin/fancy && ln -s ./fancy-exe /usr/local/bin/fancy
+rm /usr/local/bin/fancy ; ln -s ./fancy-exe /usr/local/bin/fancy
 
 echo "✨ fancy-cli installed! Use: fancy --help"
