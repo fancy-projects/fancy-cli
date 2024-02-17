20739430ff63f5dd5ca5fe5fe31fe3d5e04e8075
Zhisen <zhisen.urgolites@gmail.com>
2024-02-16 19:02:59 -0800

    Smash bugs 💥

--- install.sh
+++
@@ -1,3 +0,0 @@
-rm /usr/local/share/fancy ; cp -r ~/PycharmProjects/fancy/scripts/fancy /usr/local/share/fancy
-rm /usr/local/bin/fancy ; ln -s /usr/local/share/fancy /usr/local/bin/fancy
-echo 'done'
